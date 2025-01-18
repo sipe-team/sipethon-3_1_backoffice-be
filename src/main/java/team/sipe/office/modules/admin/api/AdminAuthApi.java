163e3e43762ec0482663dfc81757be072bfa98cd
woozi <kwj1270@naver.com>
2025-01-18 14:33:31 +0900

    feat(kwj1270): 사용자 정보 복호화 및 암호화 테스트 기능 구현 완료

--- src/main/java/team/sipe/office/modules/admin/api/AdminAuthApi.java
+++ src/main/java/team/sipe/office/modules/admin/api/AdminAuthApi.java
@@ -1,13 +1,17 @@
 package team.sipe.office.modules.admin.api;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.sipe.office.modules.admin.api.dto.AdminLoginRequest;
 import team.sipe.office.modules.admin.application.AdminAuthService;
+import team.sipe.office.modules.admin.domain.Admin;
 
-@RestController("/api/admin/auth")
+@RequestMapping("/api/admin/auth")
+@RestController
 public class AdminAuthApi {
 
     private final AdminAuthService adminAuthService;
@@ -18,9 +22,12 @@ public class AdminAuthApi {
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(
-            @RequestBody final AdminLoginRequest request
+            @RequestBody final AdminLoginRequest request,
+            final HttpSession httpSession
     ) {
-        adminAuthService.login(request.command());
+        final Admin admin = adminAuthService.login(request.command());
+        httpSession.setAttribute("id", admin.getId());
+        httpSession.setMaxInactiveInterval(24 * 60 * 60);
         return ResponseEntity.ok().build();
     }
 }
